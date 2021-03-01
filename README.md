4305b724cd49859c8faedb4bc88f288aae1c677e
Diamond Lee <diamolee55@gmail.com>
2021-03-01 00:22:56 -0500

    sorry

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# work-day-scheduler
+I'm sorry. I couldnt figure it out. To be honest I dont 
+really understand JQuery. I didn't have time to ask for help this last week. I'll do better next time. 
+Thank you.
+
--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 
-
-var time = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").text(time);
-
-$(".saveBtn").on("click", function () {
-    console.log(this);
-    var text = $(this).siblings(".description").val(); 
-    var time = $(this).parent().attr("id"); 
-    localStorage.setItem(time, text);
-})
-
+// 
