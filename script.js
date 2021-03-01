b851f9e2c17a938ad791e9f48ff30df52527ad73
Diamond Lee <diamolee55@gmail.com>
2021-02-28 19:17:32 -0500

    click function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+
+
 var time = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").text(time);
 
-$(document).ready(function () {
-    
-    })
+$(".saveBtn").on("click", function () {
+    console.log(this);
+    var text = $(this).siblings(".description").val(); 
+    var time = $(this).parent().attr("id"); 
+    localStorage.setItem(time, text);
+})
+
